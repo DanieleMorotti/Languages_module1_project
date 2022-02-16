241f67e2c04bec86b43651b9afababbbd2793a76
DanieleMorotti <daniele.morotti.99@gmail.com>
2022-02-16 09:35:55 +0100

    Script refactoring

--- Nurikabe/nurikabe_execution.py
+++ Nurikabe/nurikabe_execution.py
@@ -1,14 +1,12 @@
 #from minizinc import Instance, Model, Solver
 import argparse
 import pymzn
-import time
-import argparse
-import math
+import time, math
 
 
 def multi_puzzle(test_name: str) :
-    input = pymzn.dzn2dict(f"./tests/multi/{test_name}.dzn")
-    init : list = input["init"]
+    input_data = pymzn.dzn2dict(f"./tests/multi/{test_name}.dzn")
+    init : list = input_data["init"]
     cells_found = {}
     
     for i in range(0, len(init)):
@@ -23,37 +21,37 @@ def multi_puzzle(test_name: str) :
                     cells_found[val] = 1
                 init[i][j] = val * 10 + inc
 
-    input["digits_encoding"] = 2
-    return input
+    input_data["digits_encoding"] = 2
+    return input_data
 
 def single_puzzle(test_name: str) :
-    input = pymzn.dzn2dict(f"./tests/single/{test_name}.dzn")
-    input["digits_encoding"] = 1
-    return input
+    input_data = pymzn.dzn2dict(f"./tests/single/{test_name}.dzn")
+    input_data["digits_encoding"] = 1
+    return input_data
                 
-def optimize_bound(input: dict):      
+def optimize_bound(input_data: dict):      
     bound = set()
-    init : list = input["init"]
+    init : list = input_data["init"]
     
     for row in init:
         for val in row:
             bound.add(val)
 
-    input["cells"] = bound
-    return input
+    input_data["cells"] = bound
+    return input_data
 
 
-def default_bound(input: dict):
-    max_value = max([max(row) for row in input["init"]])
+def default_bound(input_data: dict):
+    max_value = max([max(row) for row in input_data["init"]])
     bound = 10 if max_value <= 9 else 100
 
-    input["cells"] = set(range(0, bound))
-    return input
+    input_data["cells"] = set(range(0, bound))
+    return input_data
 
-def execute_model(input: dict):
+def execute_model(input_data: dict):
     print("\nComputing the solution ... \n")
     start_time = time.time() 
-    output = pymzn.minizinc("./nurikabe_python.mzn", data=input, output_mode="dict", output_vars=["solution"], two_pass=0)
+    output = pymzn.minizinc("./nurikabe_python.mzn", data=input_data, output_mode="dict", output_vars=["solution"], two_pass=0)
     end_time = time.time() 
 
     print("The solution is:")
@@ -61,7 +59,7 @@ def execute_model(input: dict):
         for sol in output[0].values():
             for row in sol:
                 for val in row:
-                    pow_digit = pow(10, input["digits_encoding"] - 1)
+                    pow_digit = pow(10, input_data["digits_encoding"] - 1)
                     print(val // pow_digit, end="   ")
                 print("\n")
     except IndexError:
@@ -72,7 +70,7 @@ def execute_model(input: dict):
 def main():
     parser = argparse.ArgumentParser(description='Process some integers.')
     parser.add_argument('testname', metavar='FILE', type=str,
-                    help='.dzn file to test')
+                    help='Name of .dzn file to test')
     parser.add_argument('--o', dest='optimize', action='store_const',
                     const=optimize_bound, default=default_bound,
                     help='Optimize the search by adding a specific bound to the solution')
@@ -81,7 +79,6 @@ def main():
                     help='Editing the starting map to solve the puzzle with islands of same size')
     args = parser.parse_args()
     data_input = args.optimize(args.multi(args.testname))
-
     execute_model(data_input)
     # try:
     #     test_name = sys.argv[1]
